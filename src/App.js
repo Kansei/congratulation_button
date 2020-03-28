7278e98fff81094eb81015ebc52d9e19deb60130
Kansei <kk7221dev@gmail.com>
2020-03-28 16:59:41 +0900

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactPlayer from "react-player";
+import "./App.css";
 
 function App() {
+  const [playing, setPlaying] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <div className="container">
+        <div className="title">どうしても盛り上がってしまう</div>
+        <div className="title-big">ボタン</div>
+
+        <div className="buttons">
+
+        <div
+          className="button"
+          onClick={() => {
+            setIsOpen(true);
+            setPlaying(true);
+            setInterval(() => {
+              setPlaying(false);
+              setIsOpen(false);
+            }, 7000);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          PUSH START
+        </div>
+
+        <div
+          className="money"
+        >
+          課金
+        </div>
+        </div>
+
+        <ReactPlayer
+          className="player"
+          url="/congratulations.m4a"
+          playing={playing}
+          loop={false}
+        />
+      </div>
+
+      <div className={["congra", isOpen ? "show" : ""].join(" ")}>
+        <img src="/congraturation_image.png" alt="congra" className="congra-image"/>
+      </div>
     </div>
   );
 }
